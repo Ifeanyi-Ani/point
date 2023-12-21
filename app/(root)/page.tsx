fdf8ba8da6f6fdf1eb2326e99ee90eab824473d2
Ifeanyi-Ani <ifeanyiani155@gmail.com>
2023-12-21 10:22:14 +0100

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
-import { UserButton } from '@clerk/nextjs';
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+import PointCard from "@/components/cards/PointCard";
+import { fetchPoints } from "@/lib/actions/point.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchPoints(
+    searchParams.page ? +searchParams.page : 1,
+    30,
+  );
   return (
     <div>
-      <h1>Home</h1>
-      <UserButton afterSignOutUrl="/" />
+      <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">hello</section>
     </div>
   );
 }
+export default Home;
--- lib/actions/point.actions.ts
+++ lib/actions/point.actions.ts
@@ -16,7 +16,31 @@ export async function createPoint({ text, author, communityId, path }: Params) {
   try {
     connectToDB();
     const newPoint = await Point.create({ text, author });
+    await User.findByIdAndUpdate(author, {
+      $push: { points: newPoint._id },
+    });
+    revalidatePath(path);
   } catch (error: any) {
     throw new Error(`Failed create Point ${error.message}`);
+  }
+}
+export async function fetchPoints(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const pointQuery = Point.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: "User" });
+
+    const totalPointCount = await Point.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+    const points = await pointQuery.exec();
+    const isNext = totalPointCount > skipAmount + points.length;
+  } catch (error: any) {
+    throw new Error(`Error fetching Points ${error.message}`);
   }
 }
